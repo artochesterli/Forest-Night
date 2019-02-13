80561b42d547cf92d017e4d2159a2c6676024ee2
Chang <cl4929@nyu.edu>
2019-02-12 21:02:00 -0500

    gg

---
+++ Stealth Prototype 1/Assets/Dash_To_Fairy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash_To_Fairy : MonoBehaviour
+{
+
+    public bool detect_float_fairy;
+    public float dash_distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void lock_fairy()
+    {
+        GameObject fairy = GetComponent<Main_Character>().Fairy;
+        float current_dis = ((Vector2)(transform.position) - (Vector2)(fairy.transform.position)).magnitude;
+        if (current_dis <= dash_distance)
+        {
+
+        }
+    }
+
+}
---
+++ Stealth Prototype 1/Assets/Main_Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Character : MonoBehaviour
+{
+    public GameObject Fairy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fairy = GameObject.Find("Fairy").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
