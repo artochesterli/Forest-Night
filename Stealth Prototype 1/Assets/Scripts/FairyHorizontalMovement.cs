e78918972762168bb7a4d33842d2c84c3fcf00c6
Chang <cl4929@nyu.edu>
2019-05-01 15:39:09 -0400

    Update

--- Stealth Prototype 1/Assets/Scripts/FairyHorizontalMovement.cs
+++ Stealth Prototype 1/Assets/Scripts/FairyHorizontalMovement.cs
@@ -15,7 +15,7 @@ public class FairyHorizontalMovement : MonoBehaviour
     private Player player;
 
     private const float FastMoveVectorThreshold = 1f;
-    private const float SlowMoveVectorThreshold = 0.3f;
+    private const float SlowMoveVectorThreshold = 0.4f;
     // Start is called before the first frame update
     void Start()
     {
