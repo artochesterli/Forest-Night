b14336fbd7663a74a59d6c6104f9ef67b8167a32
Chang <cl4929@nyu.edu>
2019-05-06 13:08:36 -0400

    Clean Folder and change mirror totem

--- Stealth Prototype 1/Assets/Scripts/MirrorTotem.cs
+++ Stealth Prototype 1/Assets/Scripts/MirrorTotem.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MirrorTotem : MonoBehaviour
 {
     public List<GameObject> ConnectedMirrors;
+
+    private const float LightingTime = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +23,34 @@ public class MirrorTotem : MonoBehaviour
     {
         if (collision.GetComponent<Collider2D>().gameObject.CompareTag("Slash"))
         {
+            StopAllCoroutines();
+            StartCoroutine(Blink());
             for(int i = 0; i < ConnectedMirrors.Count; i++)
             {
-                ConnectedMirrors[i].GetComponent<MirrorStateManager>().Activated = !ConnectedMirrors[i].GetComponent<MirrorStateManager>().Activated;
+                var MirrorStateManager = ConnectedMirrors[i].GetComponent<MirrorStateManager>();
+                MirrorStateManager.Activated = !MirrorStateManager.Activated;
             }
+        }
+    }
+
+    private IEnumerator Blink()
+    {
+        GameObject MirrorLight = transform.Find("ActivatedLight").gameObject;
+
+        float timecount = 0;
+        while (timecount < LightingTime)
+        {
+            MirrorLight.GetComponent<SpriteRenderer>().color = Color.Lerp(new Color(1, 1, 1, 0), Color.white, timecount / LightingTime);
+            timecount += Time.deltaTime;
+            yield return null;
+        }
+
+        timecount = 0;
+        while (timecount < LightingTime)
+        {
+            MirrorLight.GetComponent<SpriteRenderer>().color = Color.Lerp( Color.white, new Color(1, 1, 1, 0), timecount / LightingTime);
+            timecount += Time.deltaTime;
+            yield return null;
         }
     }
 }
