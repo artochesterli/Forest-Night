08b35ef867a4d42bdf5c04a4ac21b85f92891e8b
Chang <cl4929@nyu.edu>
2019-05-09 05:54:46 -0400

    Revert "Merge branch 'master' of https://github.com/artochesterli/Stealthgame-Prototype-1"
    This reverts commit 22c147b49b7969cc54d4aa36cf9b403089e6501c, reversing
    changes made to a5cf52b16954bea42d02932ff6337f1216518f6d.

--- Stealth Prototype 1/Assets/Scripts/Enemy_Patrol.cs
+++ Stealth Prototype 1/Assets/Scripts/Enemy_Patrol.cs
@@ -18,16 +18,6 @@ public class Enemy_Patrol : MonoBehaviour
     void Start()
     {
         Observation_Time_Count = 0;
-
-        if (Patrol_Right)
-        {
-            Observing_Right = true;
-        }
-        else
-        {
-            Observing_Right = false;
-        }
-
         if (Patrol_Speed == 0)
         {
             IsObserving = true;
