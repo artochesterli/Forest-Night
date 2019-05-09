92a74e4ee1eb0b1b3a68767c37f70316b9c531e2
Chang <cl4929@nyu.edu>
2019-05-09 02:00:06 -0400

    Change Sprite

---
+++ Stealth Prototype 1/Assets/Load.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    private float Factor;
+    private bool down;
+    private float Speed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string scene;
+        scene = GameObject.Find("LoadingLevelData").GetComponent<LoadingLevelData>().Scene;
+        StartCoroutine(LoadScene(scene));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (down)
+        {
+            Factor -= Speed * Time.deltaTime;
+            GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, new Color(1, 1, 1, 0), Factor);
+            if (Factor < 0)
+            {
+                Factor = 0;
+                down = false;
+            }
+        }
+        else
+        {
+            Factor += Speed * Time.deltaTime;
+            GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, new Color(1, 1, 1, 0), Factor);
+            if (Factor > 1)
+            {
+                Factor = 1;
+                down = true;
+            }
+        }
+    }
+
+    private IEnumerator LoadScene(string s)
+    {
+        float timecount = 0;
+
+        GameObject SceneLoadData = (GameObject)Instantiate(Resources.Load("Prefabs/GameObject/SceneLoadData"));
+        SceneLoadData.GetComponent<SceneLoadData>().FromOtherLevel = false;
+        SceneLoadData.name = "SceneLoadData";
+        DontDestroyOnLoad(SceneLoadData);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(s);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
